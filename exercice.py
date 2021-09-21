9a182eeb58555dd4cbaeec73604c727880c28ff6
NickLopz <you@example.com>
2021-09-21 11:34:57 -0400

    je navais pas push

--- exercice.py
+++ exercice.py
@@ -6,29 +6,33 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a+b+c)/3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    combinedDegrees=angle_degs+(angle_mins/60)+(angle_secs/3600)
+    return combinedDegrees*math.pi/180
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    degree = angle_rads*180/math.pi
+    minutesDegree = int((degree-math.floor(degree))*60)
+    secondesDegree = int ((degree-math.floor(degree)-minutesDegree/60)*3600)
+    return int(degree), minutesDegree, secondesDegree
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature-32)/1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature*1.8+32
 
 
 def main() -> None:
     print(f"Moyenne des nombres 2, 4, 6: {average(2.1, 4.3, 6.5)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(100, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
